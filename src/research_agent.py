c7edc64b41904977bc9f19f0cdb9518fbe5a7bc2
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-04-29 10:09:03 -0400

    fix life path key

--- src/research_agent.py
+++ src/research_agent.py
@@ -74,7 +74,6 @@ async def run_agent(user_prompt_params: dict = {"town_name": "Batavia", "town_st
     result = await agent_chain.ainvoke(
         {"messages": [("user", PromptTemplate.from_file(prompt_dir / "user_prompt.md").format(**user_prompt_params))]}
     )
-    print(result["structured_response"])
     
     if "structured_response" in result:
         return result["structured_response"]
