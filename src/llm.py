50107114f8dc002809c268c07977f9c5cbd7d8f6
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-04-30 21:03:27 -0400

    add llm

---
+++ src/llm.py
@@ -0,0 +1,35 @@
+    
+import os
+from typing import Type
+from zipfile import Path
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage
+
+def chat_ollama_with_structured_output(user_prompt_params, system_prompt_params, return_class, prompt_dir: Path):
+
+    model = ChatOllama(
+        model=os.environ["RESEARCH_AGENT_MODEL"],
+        reasoning=True,
+        temperature=0,
+    )
+    model_structured = model.with_structured_output(return_class)
+    
+    system_prompt = PromptTemplate.from_file(prompt_dir / "sys_prompt.md").format(**system_prompt_params)
+    user_prompt = PromptTemplate.from_file(prompt_dir / "user_prompt.md").format(**user_prompt_params)
+    
+    print("System Prompt:", system_prompt)
+    print("User Prompt:", user_prompt)
+    
+    messages = [
+        SystemMessage(content=system_prompt,),
+        HumanMessage(content=user_prompt,)
+    ]
+
+    response = model.invoke(messages)
+    
+    print(response['message']['content'])
+    
+    print("Raw response from Ollama:", response)
+    
+    return response
--- src/tasks/video_gen/MuseTalk/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "entrypoint.sh"
-whoami
-which python
-source /opt/conda/etc/profile.d/conda.sh
-conda activate musev
-which python
-python app.py
