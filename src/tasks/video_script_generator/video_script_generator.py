b4d42e307c111e0cada57eba1d4bd420e129bc8b
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-04-30 21:40:16 -0400

    add llm

--- src/tasks/video_script_generator/video_script_generator.py
+++ src/tasks/video_script_generator/video_script_generator.py
@@ -47,7 +47,7 @@ def main(weekend_id=0, town_id=0):
     t = session.query(Towns).filter(Towns.id==town_id).first()
     
     chat_ollama_with_structured_output(
-        user_prompt_params={"town_name": t.name, "state": t.state, "weekend_date": w.date, "event_list":json.dumps([e._mapping for e in events]), "media_list": json.dumps([m._mapping for m in media])  },
+        user_prompt_params={"town_name": t.name, "state": t.state, "weekend_date": w.date, "event_list":json.dumps([e._asdict() for e in events]), "media_list": json.dumps([m._asdict() for m in media])  },
         system_prompt_params={}, 
         return_class=VideoSegmentsList, 
         prompt_dir=Path(__file__).parent.resolve()
