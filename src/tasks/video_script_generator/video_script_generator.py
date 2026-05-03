047c29798f3ef4a36b404c86af7c0f428f6b6327
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-02 20:54:57 -0400

    add llm

--- src/tasks/video_script_generator/video_script_generator.py
+++ src/tasks/video_script_generator/video_script_generator.py
@@ -1,5 +1,6 @@
 
 from dataclasses import asdict
+import hashlib
 import json
 
 
@@ -33,7 +34,7 @@ def populate_db_with_events(segments_list: VideoSegmentsList):
     for segment in segments_list.video_segments:
         print("Adding the segment ", segment.script_text)
         
-        file_path=f"data/audio/event_{segment.event_id}_segment_{segment.id}.wav"
+        file_path=f"data/audio/event_{segment.event_id}_segment_{hashlib.sha256(str(segment.event_id)+str(segment.timestamp)+str(segment.script_text).encode()).hexdigest()}.wav"
         
         generate_audio_file(segment.script_text, file_path=file_path)
         
