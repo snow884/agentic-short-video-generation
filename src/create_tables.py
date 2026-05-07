42d8eb45b6aec9b13ffef6c2a8faafa0de38d5e2
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-06 22:22:52 -0400

    add town

--- src/create_tables.py
+++ src/create_tables.py
@@ -3,13 +3,14 @@ import os
 
 from tables import Base
 from sql_utils import get_db, populate_towns, populate_weekends
+from pathlib import Path
 
 if __name__ == "__main__":
     
     from dotenv import load_dotenv
     from sqlalchemy import create_engine
 
-    os.rm("data/local.db") if os.path.exists("data/local.db") else None
+    os.remove("data/local.db") if os.path.exists("data/local.db") else None
 
     engine = create_engine('sqlite:///data/local.db', echo=False) # echo=True shows SQL logs
 
