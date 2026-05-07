a404cde5a18da0fb614ab0a603ccbaf3b8071c70
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-06 23:04:25 -0400

    add app.py

---
+++ src/services/SadTalker/app.py
@@ -0,0 +1,10 @@
+from .inference import main 
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/inference/")
+def read_item(args: dict):
+    save_dir = main(**args) 
+    return {"save_dir": save_dir}
