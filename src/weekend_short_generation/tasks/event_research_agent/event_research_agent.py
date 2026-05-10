6c38c745050abeaa2f2f93d27d75899e41469b3b
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-10 16:40:13 -0400

    structure in prefect format

--- src/weekend_short_generation/tasks/event_research_agent/event_research_agent.py
+++ src/weekend_short_generation/tasks/event_research_agent/event_research_agent.py
@@ -18,16 +18,21 @@ def populate_db_with_events(event_list: EventList, town_id: int, weekend_id: int
 
     session = next(get_db())
     
+    event_id_list = []
+    
     for new_event in event_list.events:
         print("Adding the event ", new_event.event_name)
         new_event_sql = Events(**new_event.__dict__)
         new_event_sql.town_id = town_id
         new_event_sql.weekend_id = weekend_id
         session.add(new_event_sql)
+        event_id_list.append(new_event_sql.id)
         
     session.commit()
 
     session.close()
+    
+    return event_id_list
 
 
 def main(town_id=0, weekend_id=0):
@@ -37,7 +42,9 @@ def main(town_id=0, weekend_id=0):
     t = session.query(Towns).filter(Towns.id==town_id).first()
     
     event_list = run_agent_sync(user_prompt_params={"town_name": t.name, "town_state": t.state, "weekend_date": w.date}, ReturnClass=EventList, prompt_dir=Path(__file__).parent.resolve())
-    populate_db_with_events(event_list, town_id=town_id, weekend_id=weekend_id)
+    event_id_list = populate_db_with_events(event_list, town_id=town_id, weekend_id=weekend_id)
+    
+    return event_id_list
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
