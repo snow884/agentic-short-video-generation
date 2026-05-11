07b1c4fa434f64fdafe94503515275b3efc70999
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-10 21:41:09 -0400

    structure in prefect format

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -3,17 +3,21 @@ from tasks.event_research_agent.event_research_agent import main as event_resear
 from tasks.image_research_agent.image_research_agent import main as image_research_agent_main
 from tasks.video_script_generator.video_script_generator import main as video_script_generator_agent_main
 from tasks.video_generator.video_generator import main as video_generator_agent_main
+from prefect import get_logger
 
 from dotenv import load_dotenv
 
 @flow(name="Weekend Short Generation Flow")
 def main_flow(weekend_id, town_id):
-        
+    
+    logger = get_logger()
+    
     load_dotenv()
     
     event_id_list = event_research_agent_main(town_id=town_id, weekend_id=weekend_id)
     
     for event_id in event_id_list:
+        logger.info(f"Processing event_id: {event_id}")
         image_research_agent_main(event_id=event_id)
         
     video_script_generator_agent_main(weekend_id=weekend_id, town_id=town_id)
