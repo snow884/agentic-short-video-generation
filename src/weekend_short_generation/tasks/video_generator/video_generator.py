7cdecdc8053ebde1c418ab6748bd7764ef68ceda
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-11 13:07:51 -0400

    fix output folders

--- src/weekend_short_generation/tasks/video_generator/video_generator.py
+++ src/weekend_short_generation/tasks/video_generator/video_generator.py
@@ -48,9 +48,9 @@ def main(weekend_id=1, town_id=1):
 
         
         if combined_video is None:
-            combined_video = clip_resized_center
+            combined_video =  CompositeVideoClip([clip_resized_center], size=(1920, 1080))
         else:
-            combined_video = concatenate_videoclips([combined_video, clip_resized_center])
+            combined_video = concatenate_videoclips([combined_video, CompositeVideoClip([clip_resized_center], size=(1920, 1080))])
     
     combined_audio_path = "data/video/sad_talker_input/combined_audio.wav"
     
