587100fe7ccf512420d08b3e64b23246bc6459e6
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-11 07:07:17 -0400

    structure in prefect format

--- src/services/SadTalker/app.py
+++ src/services/SadTalker/app.py
@@ -1,10 +1,13 @@
+from ast import Dict
+from typing import Any
+
 from inference import main 
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 @app.post("/inference/")
-def read_item(args: dict):
+def read_item(args: Dict[str, Any] = Body(...)):
     save_dir = main(**args) 
     return {"save_dir": save_dir}
