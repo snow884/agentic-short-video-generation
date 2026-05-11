928345d11bfdbea1eb7a7191abb5a12190810b78
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-11 08:01:14 -0400

    fix output folders

--- src/services/SadTalker/app.py
+++ src/services/SadTalker/app.py
@@ -7,11 +7,11 @@ from fastapi import FastAPI, Body, HTTPException
 
 app = FastAPI()
 
-@app.post("/inference")
+@app.post("/inference/")
 def inference(args: Dict[str, Any] = Body(...)):
     try:
         save_dir = main(**args) 
     except Exception as e:
-        raise HTTPException(status_code=404, detail="Sadtaalker inference failed: " + str(e))
+        raise HTTPException(status_code=400, detail="Sadtaalker inference failed: " + str(e))
     
     return {"save_dir": save_dir}
