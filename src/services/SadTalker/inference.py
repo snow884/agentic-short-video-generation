6014d8416387137f6913dfc0560b741e1a6b7df4
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-11 09:31:46 -0400

    fix output folders

--- src/services/SadTalker/inference.py
+++ src/services/SadTalker/inference.py
@@ -29,6 +29,8 @@ def main(args):
     ref_pose = args.ref_pose
 
     current_root_path = os.path.split(sys.argv[0])[0]
+    
+    print("Checkpoints path: ", args.checkpoint_dir)
 
     sadtalker_paths = init_path(args.checkpoint_dir, os.path.join(current_root_path, 'src/config'), args.size, args.old_version, args.preprocess)
     print("SadTalker paths: ", sadtalker_paths)
