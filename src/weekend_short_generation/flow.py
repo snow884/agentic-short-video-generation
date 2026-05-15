9178923a640d2d71bb28f3e94d780d303d4a256f
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-14 23:25:42 -0400

    add GC for sadtalker

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -1,7 +1,16 @@
 from dotenv import load_dotenv
 from prefect import flow
 from prefect.logging import get_run_logger
+from tasks.event_research_agent.event_research_agent import (
+    main as event_research_agent_main,
+)
 from tasks.video_generator.video_generator import main as video_generator_agent_main
+from tasks.video_parts_generator.video_parts_generator import (
+    main as video_parts_generator_agent_main,
+)
+from tasks.video_script_generator.video_script_generator import (
+    main as video_script_generator_agent_main,
+)
 
 
 @flow(name="Weekend Short Generation Flow", log_prints=True)
@@ -11,15 +20,11 @@ def main_flow(weekend_id, town_id):
 
     load_dotenv()
 
-    # event_id_list = event_research_agent_main(town_id=town_id, weekend_id=weekend_id)
+    event_id_list = event_research_agent_main(town_id=town_id, weekend_id=weekend_id)
 
-    # for event_id in event_id_list:
-    #     logger.info(f"Processing event_id: {event_id}")
-    #     image_research_agent_main(event_id=event_id)
+    video_script_generator_agent_main(weekend_id=weekend_id, town_id=town_id)
 
-    # video_script_generator_agent_main(weekend_id=weekend_id, town_id=town_id)
-
-    # video_parts_generator_agent_main(weekend_id=weekend_id, town_id=town_id)
+    video_parts_generator_agent_main(weekend_id=weekend_id, town_id=town_id)
 
     video_generator_agent_main(weekend_id=weekend_id, town_id=town_id)
 
--- src/weekend_short_generation/tasks/video_script_generator/video_script_generator.py
+++ src/weekend_short_generation/tasks/video_script_generator/video_script_generator.py
@@ -1,34 +1,21 @@
-
-from dataclasses import asdict
 import hashlib
 import json
 import os
-
-from kokoro import KPipeline
-
-import nest_asyncio
-
-from research_agent import run_agent_sync
-
-
-from sql_utils import get_db, populate_towns, populate_weekends
-from tables import Base, Events, Image, Towns, Weekends
-
+import time
 from pathlib import Path
-from tables import VideoSegmentsList, VideoSegments
-
-from llm import chat_ollama_with_structured_output
-
-from sqlalchemy import inspect
-
 
 import soundfile as sf
-import torch
 from dotenv import load_dotenv
-from prefect import flow, task
-import time 
+from kokoro import KPipeline
+from prefect import task
+from sqlalchemy import inspect
+
+from research_agent import run_agent_sync
+from sql_utils import get_db
+from tables import Events, Towns, Video, VideoSegments, VideoSegmentsList, Weekends
 
 load_dotenv()
+
 
 def object_as_dict(obj):
     return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
@@ -37,27 +24,39 @@ def object_as_dict(obj):
 def populate_db_with_events(segments_list: VideoSegmentsList):
 
     session = next(get_db())
-    
+
+    video = Video(
+        town_id=segments_list.town_id,
+        weekend_id=segments_list.weekend_id,
+        video_file_path="",
+        audio_file_path="",
+    )
+    session.add(video)
+    session.commit()
+    video_id = video.id
+
     for segment in segments_list.video_segments:
         print("Adding the segment ", segment.script_text)
-        
-        file_path=f"data/audio/event_{segment.event_id}_segment_{hashlib.sha256((str(segment.event_id)+str(segment.timestamp)+str(segment.script_text)).encode()).hexdigest()}.wav"
-        
+
+        file_path = f"data/audio/event_{segment.event_id}_segment_{hashlib.sha256((str(segment.event_id)+str(segment.timestamp)+str(segment.script_text)).encode()).hexdigest()}.wav"
+
         generate_audio_file(segment.script_text, file_path=file_path)
-        
+
         new_event_sql = VideoSegments(**segment.__dict__)
         new_event_sql.sound_file_path = file_path
+        new_event_sql.video_id = video_id
         session.add(new_event_sql)
-        
+
     session.commit()
 
     session.close()
 
-def generate_audio_file(text, file_path='your_audio_file.wav'):
-    device = 'cpu'
-    pipeline = KPipeline(lang_code='a', device=device)
 
-    generator = pipeline(text, voice='af_heart', speed=1.2)
+def generate_audio_file(text, file_path="your_audio_file.wav"):
+    device = "cpu"
+    pipeline = KPipeline(lang_code="a", device=device)
+
+    generator = pipeline(text, voice="af_heart", speed=1.2)
 
     duration = 0
 
@@ -66,8 +65,9 @@ def generate_audio_file(text, file_path='your_audio_file.wav'):
         sf.write(file_path, audio, 24000)
         info = sf.info(file_path)
         duration = duration + info.duration
-        
+
     return duration
+
 
 def check_text_spoken_length_matches_timestamps(segments_list: list):
     """
@@ -80,58 +80,140 @@ def check_text_spoken_length_matches_timestamps(segments_list: list):
         bool: True if all segments' script text length is approximately equal to the time difference from the previous segment, otherwise False.
     """
     print(f"Checking segments length relative to timestamps for {segments_list}...")
-    
+
     if not segments_list:
         return "You provided an empty value. No scripts segments to check."
-    
+
     durations = [0] * len(segments_list)
-    
+
     for i, segment in enumerate(segments_list):
-        
-        durations[i] = generate_audio_file(segment['script_text'])
-        
+
+        durations[i] = generate_audio_file(segment["script_text"])
+
         res_str = ""
-        
-        if (i < (len(segments_list) - 1)) and abs(( durations[i] / ((segment['timestamp'] - segments_list[i+1]['timestamp'])))-1)>0.05 :  # Assuming 2 words per second as a speaking rate
-            print(f"Error: Segment number {i} at timestamp {segment['timestamp']} has script takes approximately {durations[i]} seconds to speak, but the timestamp difference to the next segment at { segments_list[i+1]['timestamp']} is {abs(segment['timestamp'] - segments_list[i+1]['timestamp'])} seconds. Adjust the timestamps or script text length for better synchronization.")
-            res_str = res_str + (f"Error: Segment number {i} at timestamp {segment['timestamp']} has script takes approximately {durations[i]} seconds to speak, but the timestamp difference to the next segment number {i+1} at timestamp { segments_list[i+1]['timestamp']} is {abs(segment['timestamp'] - segments_list[i+1]['timestamp'])} seconds. Adjust the timestamps or script text length for better synchronization.")
-            res_str = res_str + "\n"
-        
-    for i, segment in enumerate(segments_list):
-        
-        if (i < (len(segments_list) - 1)) and (segment['timestamp'] >= segments_list[i+1]['timestamp']):
-            print(f"Error: Segment number {i} at timestamp {segment['timestamp']} has a timestamp that is not less than the next segment number {i+1} at timestamp { segments_list[i+1]['timestamp']}. Adjust the timestamps for better synchronization.")
-            res_str = res_str + (f"Error: Segment number {i} at timestamp {segment['timestamp']} has a timestamp that is not less than the next segment number {i+1} at timestamp { segments_list[i+1]['timestamp']}. Adjust the timestamps for better synchronization.")
+
+        if (i < (len(segments_list) - 1)) and abs(
+            (
+                durations[i]
+                / ((segment["timestamp"] - segments_list[i + 1]["timestamp"]))
+            )
+            - 1
+        ) > 0.05:  # Assuming 2 words per second as a speaking rate
+            print(
+                f"Error: Segment number {i} at timestamp {segment['timestamp']} has"
+                f" script takes approximately {durations[i]} seconds to speak, but the"
+                " timestamp difference to the next segment at"
+                f" { segments_list[i+1]['timestamp']} is"
+                f" {abs(segment['timestamp'] - segments_list[i+1]['timestamp'])} seconds."
+                " Adjust the timestamps or script text length for better"
+                " synchronization."
+            )
+            res_str = (
+                res_str
+                + f"Error: Segment number {i} at timestamp {segment['timestamp']} has"
+                f" script takes approximately {durations[i]} seconds to speak, but"
+                f" the timestamp difference to the next segment number {i+1} at"
+                f" timestamp { segments_list[i+1]['timestamp']} is"
+                f" {abs(segment['timestamp'] - segments_list[i+1]['timestamp'])} seconds."
+                " Adjust the timestamps or script text length for better"
+                " synchronization."
+            )
             res_str = res_str + "\n"
 
     for i, segment in enumerate(segments_list):
-        
+
+        if (i < (len(segments_list) - 1)) and (
+            segment["timestamp"] >= segments_list[i + 1]["timestamp"]
+        ):
+            print(
+                f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                f" timestamp that is not less than the next segment number {i+1} at"
+                f" timestamp { segments_list[i+1]['timestamp']}. Adjust the timestamps"
+                " for better synchronization."
+            )
+            res_str = (
+                res_str
+                + f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                f" timestamp that is not less than the next segment number {i+1} at"
+                f" timestamp { segments_list[i+1]['timestamp']}. Adjust the"
+                " timestamps for better synchronization."
+            )
+            res_str = res_str + "\n"
+
+    for i, segment in enumerate(segments_list):
+
         if durations[i] > 6.0 or durations[i] < 4.0:
-            print(f"Error: Segment number {i} at timestamp {segment['timestamp']} has a duration of {durations[i]} seconds which is outside the acceptable range of 4 to 6 seconds. Adjust the script text length for better synchronization.")
-            res_str = res_str + (f"Error: Segment number {i} at timestamp {segment['timestamp']} has a duration of {durations[i]} seconds which is outside the acceptable range of 4 to 6 seconds. Adjust the script text length for better synchronization.")
+            print(
+                f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                f" duration of {durations[i]} seconds which is outside the acceptable"
+                " range of 4 to 6 seconds. Adjust the script text length for better"
+                " synchronization."
+            )
+            res_str = (
+                res_str
+                + f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                f" duration of {durations[i]} seconds which is outside the acceptable"
+                " range of 4 to 6 seconds. Adjust the script text length for better"
+                " synchronization."
+            )
             res_str = res_str + "\n"
 
-        
-    if abs(segments_list[-1]['timestamp']/180-1)>0.05:
-        print(f"Error: The last segment has a timestamp of {segments_list[-1]['timestamp']} seconds which is significantly different than the expected video length of 180 seconds. Consider adjusting the timestamps or adding more segments to better utilize the video length.")
-        res_str = res_str +  "The total video length is significantly different than 180 seconds."
+    if abs(segments_list[-1]["timestamp"] / 180 - 1) > 0.05:
+        print(
+            "Error: The last segment has a timestamp of"
+            f" {segments_list[-1]['timestamp']} seconds which is significantly"
+            " different than the expected video length of 180 seconds. Consider"
+            " adjusting the timestamps or adding more segments to better utilize the"
+            " video length."
+        )
+        res_str = (
+            res_str
+            + "The total video length is significantly different than 180 seconds."
+        )
         res_str = res_str + "\n"
 
-    if (sum(durations) /180-1)>0.05:
-        print(f"Error: The total duration of all segments is {sum(durations)} seconds which is significantly different than the expected video length of 180 seconds. Consider adjusting the timestamps or script text length for better synchronization.")
-        res_str = res_str +  "The total duration of all segments is significantly different than 180 seconds."
+    if (sum(durations) / 180 - 1) > 0.05:
+        print(
+            f"Error: The total duration of all segments is {sum(durations)} seconds"
+            " which is significantly different than the expected video length of 180"
+            " seconds. Consider adjusting the timestamps or script text length for"
+            " better synchronization."
+        )
+        res_str = (
+            res_str
+            + "The total duration of all segments is significantly different than 180"
+            " seconds."
+        )
         res_str = res_str + "\n"
 
     for i, segment in enumerate(segments_list):
-        
-        if segment['scene_description'] and len(segment['scene_description']) > 400:
-            print(f"Error: Segment number {i} at timestamp {segment['timestamp']} has a scene description that is too long. Consider shortening the description for better synchronization.")
-            res_str = res_str + (f"Error: Segment number {i} at timestamp {segment['timestamp']} has a scene description that is too long. Consider shortening the description for better synchronization.")
+
+        if segment["scene_description"] and len(segment["scene_description"]) > 400:
+            print(
+                f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                " scene description that is too long. Consider shortening the"
+                " description for better synchronization."
+            )
+            res_str = (
+                res_str
+                + f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                " scene description that is too long. Consider shortening the"
+                " description for better synchronization."
+            )
             res_str = res_str + "\n"
 
-        if segment['scene_description'] and len(segment['scene_description']) < 100:
-            print(f"Error: Segment number {i} at timestamp {segment['timestamp']} has a scene description that is too short. Consider lengthening the description for better synchronization.")
-            res_str = res_str + (f"Error: Segment number {i} at timestamp {segment['timestamp']} has a scene description that is too short. Consider lengthening the description for better synchronization.")
+        if segment["scene_description"] and len(segment["scene_description"]) < 100:
+            print(
+                f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                " scene description that is too short. Consider lengthening the"
+                " description for better synchronization."
+            )
+            res_str = (
+                res_str
+                + f"Error: Segment number {i} at timestamp {segment['timestamp']} has a"
+                " scene description that is too short. Consider lengthening the"
+                " description for better synchronization."
+            )
             res_str = res_str + "\n"
 
     print("All segments have correct length relative to their timestamps.")
@@ -139,36 +221,63 @@ def check_text_spoken_length_matches_timestamps(segments_list: list):
     if res_str:
         print(res_str)
         return res_str
-    
-    return 'success'
-    
+
+    return "success"
+
+
 @task(task_run_name="video_script_generator_agent-{weekend_id}-{town_id}")
 def main(weekend_id=0, town_id=0):
     session = next(get_db())
-    
-    events = session.query(Events).filter(Events.weekend_id==weekend_id, Events.town_id==town_id).all()
+
+    events = (
+        session.query(Events)
+        .filter(Events.weekend_id == weekend_id, Events.town_id == town_id)
+        .all()
+    )
 
     if not events:
         print("No events found for the given weekend and town.")
         return
 
-    w = session.query(Weekends).filter(Weekends.id==weekend_id).first()
-    t = session.query(Towns).filter(Towns.id==town_id).first()
-    
+    w = session.query(Weekends).filter(Weekends.id == weekend_id).first()
+    t = session.query(Towns).filter(Towns.id == town_id).first()
+
     # chat_ollama_with_structured_output(
     #     user_prompt_params={"town_name": t.name, "state": t.state, "weekend_date": w.date, "event_list":json.dumps([{"name": event.event_name,"address":event.location_address, "description": event.description, "date": event.date, "time": event.time, "id": event.id} for event in events]), "Image_list": json.dumps([{"title": m.title, "description": m.description, "id": m.id, "event_id": m.event_id} for m in images])  },
-    #     system_prompt_params={}, 
-    #     return_class=VideoSegmentsList, 
+    #     system_prompt_params={},
+    #     return_class=VideoSegmentsList,
     #     prompt_dir=Path(__file__).parent.resolve()
     # )
 
-    user_prompt_params={"town_name": t.name, "state": t.state, "weekend_date": w.date, "event_list":json.dumps([{"name": event.event_name,"address":event.location_address, "description": event.description, "date": event.date, "time": event.time, "id": event.id} for event in events])  }
-    system_prompt_params={}
+    user_prompt_params = {
+        "town_name": t.name,
+        "state": t.state,
+        "weekend_date": w.date,
+        "event_list": json.dumps(
+            [
+                {
+                    "name": event.event_name,
+                    "address": event.location_address,
+                    "description": event.description,
+                    "date": event.date,
+                    "time": event.time,
+                    "id": event.id,
+                }
+                for event in events
+            ]
+        ),
+    }
+    system_prompt_params = {}
 
-    Video_Segments_List = run_agent_sync(user_prompt_params=user_prompt_params,system_prompt_params=system_prompt_params, ReturnClass=VideoSegmentsList, prompt_dir=Path(__file__).parent.resolve(), extra_tools=[check_text_spoken_length_matches_timestamps])
-    print("Received Video Segments list: ", Video_Segments_List  )
+    Video_Segments_List = run_agent_sync(
+        user_prompt_params=user_prompt_params,
+        system_prompt_params=system_prompt_params,
+        ReturnClass=VideoSegmentsList,
+        prompt_dir=Path(__file__).parent.resolve(),
+        extra_tools=[check_text_spoken_length_matches_timestamps],
+    )
+    print("Received Video Segments list: ", Video_Segments_List)
     populate_db_with_events(Video_Segments_List)
-
 
     print("clear model from vmem...")
     import ollama
@@ -179,6 +288,7 @@ def main(weekend_id=0, town_id=0):
     print(Video_Segments_List.video_segments[0])
     sfsfdsdsdssdsdds
 
+
 if __name__ == "__main__":
-    
+
     main(town_id=1, weekend_id=1)
