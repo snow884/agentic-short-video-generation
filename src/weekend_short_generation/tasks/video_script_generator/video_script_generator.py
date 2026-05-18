c27e43ecc1222b85637d29e0cf67198bfef84c96
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-18 17:23:34 -0400

    add GC for sadtalker

--- src/weekend_short_generation/tasks/video_script_generator/video_script_generator.py
+++ src/weekend_short_generation/tasks/video_script_generator/video_script_generator.py
@@ -238,6 +238,38 @@ def check_text_spoken_length_matches_timestamps(segments_list: list):
             )
             res_str = res_str + "\n"
 
+    if not segments_list[0]["event_id"] is None:
+        print(
+            "Error: The first segment has an event_id of"
+            f" {segments_list[0]['event_id']} which is not None. Set the event_id of"
+            " the first segment to None to indicate that it is an introduction segment"
+            " without a specific event."
+        )
+        res_str = (
+            res_str
+            + "Error: The first segment has an event_id of"
+            f" {segments_list[0]['event_id']} which is not None. Set the event_id of"
+            " the first segment to None to indicate that it is an introduction"
+            " segment without a specific event."
+        )
+        res_str = res_str + "\n"
+
+    if not segments_list[-1]["event_id"] is None:
+        print(
+            "Error: The last segment has an event_id of"
+            f" {segments_list[-1]['event_id']} which is not None. Set the event_id of"
+            " the last segment to None to indicate that it is a conclusion segment"
+            " without a specific event."
+        )
+        res_str = (
+            res_str
+            + "Error: The last segment has an event_id of"
+            f" {segments_list[-1]['event_id']} which is not None. Set the event_id of"
+            " the last segment to None to indicate that it is a conclusion segment"
+            " without a specific event."
+        )
+        res_str = res_str + "\n"
+
     print("All segments have correct length relative to their timestamps.")
 
     if res_str:
