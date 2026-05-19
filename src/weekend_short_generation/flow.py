6f70adcf3f7df02aa2ac1ad3039c912e33ac590d
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-19 10:48:47 -0400

    add GC for sadtalker

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -35,4 +35,4 @@ def main_flow(weekend_id, town_id_list):
 
 
 if __name__ == "__main__":
-    main_flow(weekend_id=1, town_id_list=[5, 6, 7, 8, 9])
+    main_flow(weekend_id=1, town_id_list=[10, 11, 12, 13, 14])
