8c606add6ef347b5ee1a3a70d97d0f720422f8fe
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-05-21 07:00:24 -0400

    add timestamp order

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -1,8 +1,18 @@
 from dotenv import load_dotenv
 from prefect import flow, task
 from prefect.logging import get_run_logger
+from tasks.event_research_agent.event_research_agent import (
+    main as event_research_agent_main,
+)
 from tasks.video_description_generator.video_description_generator import (
     main as video_description_generator_agent_main,
+)
+from tasks.video_generator.video_generator import main as video_generator_agent_main
+from tasks.video_parts_generator.video_parts_generator import (
+    main as video_parts_generator_agent_main,
+)
+from tasks.video_script_generator.video_script_generator import (
+    main as video_script_generator_agent_main,
 )
 
 from sql_utils import get_db
@@ -43,26 +53,24 @@ def main_flow(weekend_id, town_id_list):
 
     for town_id in town_id_list:
 
-        # video_id = create_video(weekend_id, town_id)
+        video_id = create_video(weekend_id, town_id)
 
-        video_id = 1
+        # video_id = 1
 
-        # logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
+        logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
 
-        # event_id_list = event_research_agent_main(
-        #     town_id=town_id, weekend_id=weekend_id
-        # )
+        event_id_list = event_research_agent_main(
+            town_id=town_id, weekend_id=weekend_id
+        )
 
-        # video_script_generator_agent_main(video_id)
+        video_script_generator_agent_main(video_id)
 
-        # video_parts_generator_agent_main(video_id)
+        video_parts_generator_agent_main(video_id)
 
-        # video_generator_agent_main(video_id)
+        video_generator_agent_main(video_id)
 
         video_description_generator_agent_main(video_id)
 
-        dfsfdsfsfs
-
 
 if __name__ == "__main__":
-    main_flow(weekend_id=1, town_id_list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    main_flow(weekend_id=1, town_id_list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
