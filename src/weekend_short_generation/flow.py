5f6e4d3759485b55d9142cc6ecea16f084ef05c6
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-06-07 06:07:39 -0400

    add workflow jsons

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -10,13 +10,13 @@ from tasks.video_description_generator.video_description_generator import (
     main as video_description_generator_agent_main,
 )
 from tasks.video_generator.video_generator import main as video_generator_agent_main
+from tasks.video_parts_generator.video_parts_generator import (
+    main as video_parts_generator_agent_main,
+)
 
 from sql_utils import get_db
 from tables import Towns, Video, Weekends
 
-# from tasks.video_parts_generator.video_parts_generator import (
-#     main as video_parts_generator_agent_main,
-# )
 # from tasks.video_script_generator.video_script_generator import (
 #     main as video_script_generator_agent_main,
 # )
@@ -68,7 +68,7 @@ def main_flow(weekend_id, town_id_list):
 
         # video_script_generator_agent_main(video_id)
 
-        # video_parts_generator_agent_main(video_id)
+        video_parts_generator_agent_main(video_id)
 
         video_generator_agent_main(video_id)
 
