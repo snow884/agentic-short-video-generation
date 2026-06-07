e92c072a9f06f413719fd2d4c9974ff40d55f68a
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-06-06 20:47:44 -0400

    add workflow jsons

--- src/test.py
+++ src/test.py
@@ -1,19 +1,14 @@
-from pathlib import Path
-
 from weekend_short_generation.tasks.video_parts_generator.video_parts_generator import (
     run_comfyui_workflow,
 )
 
-if __name__ == "__main__":
+
+def generate_video(prompt, output_file_path):
 
     workflow_file = "wan2_2_t2v_lightx2v_lora_distorch.json"
 
     def positive_prompt_modification(node):
-        node["inputs"]["text"] = (
-            "An ugly man dressed all in dirty brown clothes farts in an elevator,"
-            " causing a group of professionally dressed people to look at him in"
-            " disgust."
-        )
+        node["inputs"]["text"] = prompt
         print(f"new prompt: {node['inputs']['text']}")
         return node
 
@@ -31,7 +26,5 @@ if __name__ == "__main__":
         "3": positive_prompt_modification,
         "4": negative_prompt_modification,
     }
-
-    output_file_path = Path(__file__).parent.resolve() / "test_output_video.mp4"
 
     run_comfyui_workflow(workflow_file, output_file_path, prompt_modifications)
