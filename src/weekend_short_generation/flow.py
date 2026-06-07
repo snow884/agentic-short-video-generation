9845aabc563d627fad47403741c88c79cfcafe38
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-06-07 06:21:52 -0400

    add workflow jsons

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -56,17 +56,17 @@ def main_flow(weekend_id, town_id_list):
 
     for town_id in town_id_list:
 
-        # video_id = create_video(weekend_id, town_id)
+        video_id = create_video(weekend_id, town_id)
 
-        video_id = 1
+        # video_id = 1
 
-        # logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
+        logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
 
-        # event_id_list = event_research_agent_main(
-        #     town_id=town_id, weekend_id=weekend_id
-        # )
+        event_id_list = event_research_agent_main(
+            town_id=town_id, weekend_id=weekend_id
+        )
 
-        # video_script_generator_agent_main(video_id)
+        video_script_generator_agent_main(video_id)
 
         video_parts_generator_agent_main(video_id)
 
@@ -75,7 +75,6 @@ def main_flow(weekend_id, town_id_list):
         video_description_generator_agent_main(video_id)
 
         subtitle_gen_agent_main(video_id)
-        sadsdd
 
 
 if __name__ == "__main__":
