74f0fe40e8c6bd9bcdf50d438474bc2b273c82cd
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-06-09 12:31:31 -0400

    Fix negatime prompt

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 from prefect import flow, task
 from prefect.logging import get_run_logger
+from tasks.event_research_agent.event_research_agent import (
+    main as event_research_agent_main,
+)
 from tasks.subtitle_file_generator.subtitle_gen import main as subtitle_gen_agent_main
 from tasks.video_description_generator.video_description_generator import (
     main as video_description_generator_agent_main,
@@ -8,6 +11,9 @@ from tasks.video_description_generator.video_description_generator import (
 from tasks.video_generator.video_generator import main as video_generator_agent_main
 from tasks.video_parts_generator.video_parts_generator import (
     main as video_parts_generator_agent_main,
+)
+from tasks.video_script_generator.video_script_generator import (
+    main as video_script_generator_agent_main,
 )
 
 from sql_utils import get_db
@@ -54,11 +60,11 @@ def main_flow(weekend_id, town_id_list):
 
         logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
 
-        # event_id_list = event_research_agent_main(
-        #     town_id=town_id, weekend_id=weekend_id
-        # )
+        event_id_list = event_research_agent_main(
+            town_id=town_id, weekend_id=weekend_id
+        )
 
-        # video_script_generator_agent_main(video_id)
+        video_script_generator_agent_main(video_id)
 
         video_parts_generator_agent_main(video_id)
 
