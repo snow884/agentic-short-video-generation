eb622c671eb998af9d49569770b8f1ed1ec31c35
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-06-11 22:00:01 -0400

    Fix negatime prompt

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -1,10 +1,9 @@
 from dotenv import load_dotenv
 from prefect import flow, task
 from prefect.logging import get_run_logger
-
-# from tasks.event_research_agent.event_research_agent import (
-#    main as event_research_agent_main,
-# )
+from tasks.event_research_agent.event_research_agent import (
+    main as event_research_agent_main,
+)
 from tasks.subtitle_file_generator.subtitle_gen import main as subtitle_gen_agent_main
 from tasks.video_description_generator.video_description_generator import (
     main as video_description_generator_agent_main,
@@ -13,13 +12,12 @@ from tasks.video_generator.video_generator import main as video_generator_agent_
 from tasks.video_parts_generator.video_parts_generator import (
     main as video_parts_generator_agent_main,
 )
+from tasks.video_script_generator.video_script_generator import (
+    main as video_script_generator_agent_main,
+)
 
 from sql_utils import get_db
 from tables import Towns, Video, Weekends
-
-# from tasks.video_script_generator.video_script_generator import (
-#    main as video_script_generator_agent_main,
-# )
 
 
 @task(task_run_name="create_video-{weekend_id}-{town_id}")
@@ -56,17 +54,17 @@ def main_flow(weekend_id, town_id_list):
 
     for town_id in town_id_list:
 
-        # video_id = create_video(weekend_id, town_id)
+        video_id = create_video(weekend_id, town_id)
 
-        video_id = 13
+        # video_id = 13
 
         logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
 
-        # event_id_list = event_research_agent_main(
-        #    town_id=town_id, weekend_id=weekend_id
-        # )
+        event_id_list = event_research_agent_main(
+            town_id=town_id, weekend_id=weekend_id
+        )
 
-        # video_script_generator_agent_main(video_id)
+        video_script_generator_agent_main(video_id)
 
         video_parts_generator_agent_main(video_id)
 
