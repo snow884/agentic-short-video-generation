ee44da2dea41d35051fe85203feb2df6f2ffa9c1
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-06-27 19:28:42 -0400

    add tiktok cookie

--- src/weekend_short_generation/tasks/upload_video/upload_tiktok.py
+++ src/weekend_short_generation/tasks/upload_video/upload_tiktok.py
@@ -17,8 +17,10 @@ def main(video_id):
 
     video_file_path = video.video_file_path
 
-    uploader = TikTokUploader(cookies="www.tiktok.com_cookies.txt")
-    uploader.upload_video(video_file_path, description=video.description, headless=True)
+    uploader = TikTokUploader(
+        cookies="www.tiktok.com_cookies.txt", headless=True, browser="chromium"
+    )
+    uploader.upload_video(video_file_path, description=video.description)
 
     print("Video upload triggered successfully!")
 
