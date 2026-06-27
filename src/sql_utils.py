5cd9dea51af843f9ce81d94b2ea8b0d28e9e5693
Adam Ivansky <adam.ivansky@gmail.com>
2026-06-27 23:05:55 +0000

    modify flow.py

--- src/sql_utils.py
+++ src/sql_utils.py
@@ -30,7 +30,7 @@ def populate_weekends():
         return
 
     # Example: Populate weekends with some dummy data
-    weekends = [Weekends(date="2026-06-20")]
+    weekends = [Weekends(date="2026-06-27")]
 
     for weekend in weekends:
         session.add(weekend)
