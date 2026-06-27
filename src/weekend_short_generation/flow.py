cda0bbdf8f4c4bd852f04e05c02de1918c088586
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-06-27 09:35:28 -0400

    add tiktok cookie

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -1,23 +1,25 @@
 from dotenv import load_dotenv
 from prefect import flow, task
 from prefect.logging import get_run_logger
-from tasks.event_research_agent.event_research_agent import (
-    main as event_research_agent_main,
-)
-from tasks.subtitle_file_generator.subtitle_gen import main as subtitle_gen_agent_main
-from tasks.video_description_generator.video_description_generator import (
-    main as video_description_generator_agent_main,
-)
-from tasks.video_generator.video_generator import main as video_generator_agent_main
-from tasks.video_parts_generator.video_parts_generator import (
-    main as video_parts_generator_agent_main,
-)
-from tasks.video_script_generator.video_script_generator import (
-    main as video_script_generator_agent_main,
-)
+from tasks.upload_video.upload_tiktok import main as upload_tiktok_agent_main
 
 from sql_utils import get_db
 from tables import Towns, Video, Weekends
+
+# from tasks.event_research_agent.event_research_agent import (
+#     main as event_research_agent_main,
+# )
+# from tasks.subtitle_file_generator.subtitle_gen import main as subtitle_gen_agent_main
+# from tasks.video_description_generator.video_description_generator import (
+#     main as video_description_generator_agent_main,
+# )
+# from tasks.video_generator.video_generator import main as video_generator_agent_main
+# from tasks.video_parts_generator.video_parts_generator import (
+#     main as video_parts_generator_agent_main,
+# )
+# from tasks.video_script_generator.video_script_generator import (
+#     main as video_script_generator_agent_main,
+# )
 
 
 @task(task_run_name="create_video-{weekend_id}-{town_id}")
@@ -54,9 +56,9 @@ def main_flow(weekend_id, town_id_list):
 
     for town_id in town_id_list:
 
-        video_id = create_video(weekend_id, town_id)
+        # video_id = create_video(weekend_id, town_id)
 
-        # video_id = 13
+        video_id = 49
 
         logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
 
@@ -64,15 +66,17 @@ def main_flow(weekend_id, town_id_list):
             town_id=town_id, weekend_id=weekend_id
         )
 
-        video_script_generator_agent_main(video_id)
+        # video_script_generator_agent_main(video_id)
 
-        video_parts_generator_agent_main(video_id)
+        # video_parts_generator_agent_main(video_id)
 
-        video_generator_agent_main(video_id)
+        # video_generator_agent_main(video_id)
 
-        video_description_generator_agent_main(video_id)
+        # video_description_generator_agent_main(video_id)
 
-        subtitle_gen_agent_main(video_id)
+        # subtitle_gen_agent_main(video_id)
+        upload_tiktok_agent_main(video_id)
+        adsddsadas
 
 
 if __name__ == "__main__":
