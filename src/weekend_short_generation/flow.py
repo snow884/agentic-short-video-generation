52c3ec237217452b135402ae9d68eb15b77adf0d
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-07-03 06:17:39 -0400

    add md file

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -1,22 +1,24 @@
 from dotenv import load_dotenv
 from prefect import flow, task
 from prefect.logging import get_run_logger
-
-# from tasks.event_research_agent.event_research_agent import (
-#     main as event_research_agent_main,
-# )
+from tasks.event_research_agent.event_research_agent import (
+    main as event_research_agent_main,
+)
+from tasks.subtitle_file_generator.subtitle_gen import main as subtitle_gen_agent_main
 from tasks.upload_video.upload_video import main as upload_video_main
+from tasks.video_description_generator.video_description_generator import (
+    main as video_description_generator_agent_main,
+)
+from tasks.video_generator.video_generator import main as video_generator_agent_main
+from tasks.video_parts_generator.video_parts_generator import (
+    main as video_parts_generator_agent_main,
+)
+from tasks.video_script_generator.video_script_generator import (
+    main as video_script_generator_agent_main,
+)
 
 from sql_utils import get_db
 from tables import Towns, Video, Weekends
-
-# from tasks.video_generator.video_generator import main as video_generator_agent_main
-# from tasks.video_parts_generator.video_parts_generator import (
-#     main as video_parts_generator_agent_main,
-# )
-# from tasks.video_script_generator.video_script_generator import (
-#     main as video_script_generator_agent_main,
-# )
 
 
 @task(task_run_name="create_video-{weekend_id}-{town_id}")
@@ -53,39 +55,38 @@ def main_flow(weekend_id, town_id_list):
 
     for town_id in town_id_list:
 
-        # video_id = create_video(weekend_id, town_id)
+        video_id = create_video(weekend_id, town_id)
 
-        session = next(get_db())
+        # session = next(get_db())
 
-        video_id = (
-            session.query(Video)
-            .filter(
-                Video.weekend_id == weekend_id,
-                Video.town_id == town_id,
-                Video.video_file_path != "",
-            )
-            .first()
-            .id
-        )
-
-        # logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
-
-        # event_id_list = event_research_agent_main(
-        #     town_id=town_id, weekend_id=weekend_id
+        # video_id = (
+        #     session.query(Video)
+        #     .filter(
+        #         Video.weekend_id == weekend_id,
+        #         Video.town_id == town_id,
+        #         Video.video_file_path != "",
+        #     )
+        #     .first()
+        #     .id
         # )
 
-        # video_script_generator_agent_main(video_id)
+        logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
 
-        # video_parts_generator_agent_main(video_id)
+        event_id_list = event_research_agent_main(
+            town_id=town_id, weekend_id=weekend_id
+        )
 
-        # video_generator_agent_main(video_id)
+        video_script_generator_agent_main(video_id)
 
-        # video_description_generator_agent_main(video_id)
+        video_parts_generator_agent_main(video_id)
 
-        # subtitle_gen_agent_main(video_id)
+        video_generator_agent_main(video_id)
+
+        video_description_generator_agent_main(video_id)
+
+        subtitle_gen_agent_main(video_id)
 
         upload_video_main(video_id)
-        jklkjlljkljk
 
 
 if __name__ == "__main__":
