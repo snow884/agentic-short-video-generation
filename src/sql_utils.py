5b9a3fa12c184d2ef6d9d2823fc4d015ac96fd60
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-07-04 22:29:17 -0400

    add md file

--- src/sql_utils.py
+++ src/sql_utils.py
@@ -36,7 +36,7 @@ def populate_weekends():
         return
 
     # Example: Populate weekends with some dummy data
-    weekends = [Weekends(date="2026-07-04")]
+    weekends = [Weekends(date="2026-07-11")]
 
     for weekend in weekends:
         session.add(weekend)
