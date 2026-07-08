245073bc0208aef260b674a15180f05f4509b7a4
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-07-07 22:05:28 -0400

    add md file

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -3,6 +3,9 @@
 from dotenv import load_dotenv
 from prefect import flow, task
 from prefect.logging import get_run_logger
+from tasks.event_research_agent.event_research_agent import (
+    main as event_research_agent_main,
+)
 from tasks.subtitle_file_generator.subtitle_gen import main as subtitle_gen_agent_main
 from tasks.upload_video.upload_video import main as upload_video_main
 from tasks.upload_video_analytics.collect_video_analytics import (
@@ -82,10 +85,10 @@ def main_flow(weekend_id, town_id_list):
 
         logger.info(f"Processing town_id: {town_id} for weekend_id: {weekend_id}")
 
-        # event_id_list = event_research_agent_main(
-        #     town_id=town_id, weekend_id=weekend_id
-        # )
-        event_id_list = [1]
+        event_id_list = event_research_agent_main(
+            town_id=town_id, weekend_id=weekend_id
+        )
+        # event_id_list = [1]
         for event_id in event_id_list:
 
             logger.info(f"Event ID: {event_id}")
