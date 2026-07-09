aab5c46e7c96b6e51f9d80d54c27b8d0785965d7
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-07-09 16:10:30 -0400

    add md file

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -113,4 +113,4 @@ def main_flow(weekend_id, town_id_list):
 
 
 if __name__ == "__main__":
-    main_flow(weekend_id=1, town_id_list=[i for i in range(3, 49)])
+    main_flow(weekend_id=1, town_id_list=[i for i in range(4, 49)])
