f9d0229fb593df5304bf5ffd5f2904ee003705e7
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-07-12 21:55:19 -0400

    add Db inspector

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -93,7 +93,7 @@ def main_flow(weekend_id, town_id_list):
 
             video_id = create_video(weekend_id, town_id)
 
-            video_id = 1
+            # video_id = 1
 
             # video_script_generator_agent_main(video_id, event_id)
 
