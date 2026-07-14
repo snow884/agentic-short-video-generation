e0433c14f4d0cb822d34a2ace94c4f4e4e101a71
snow884 <92733886+aivansky-contractor@users.noreply.github.com>
2026-07-13 22:08:44 -0400

    add Db inspector

--- src/weekend_short_generation/flow.py
+++ src/weekend_short_generation/flow.py
@@ -90,12 +90,12 @@ def main_flow(weekend_id, town_id_list):
         # event_id_list = event_research_agent_main(
         #     town_id=town_id, weekend_id=weekend_id
         # )
-        event_id_list = [0]
+        event_id_list = [1]
         for event_id in event_id_list:
 
             logger.info(f"Event ID: {event_id}")
 
-            # video_id = create_video(weekend_id, town_id)
+            video_id = create_video(weekend_id, town_id)
 
             video_id = 1
 
